cf45052948ca9a42075ad29936d5d9bc22c6a0b7
QGL <2746343370@qq.com>
2020-11-25 16:00:27 +0800

    Add entity,dao

---
+++ src/main/java/edu/nf/eshop/dao/ProducDao.java
@@ -0,0 +1,7 @@
+package edu.nf.eshop.dao;
+
+public class ProducDao {
+    public void save(Process process){
+
+    }
+}
---
+++ src/main/java/edu/nf/eshop/entity/Product.java
@@ -0,0 +1,10 @@
+package edu.nf.eshop.entity;
+
+import java.math.BigDecimal;
+
+
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+}
